2525821a6d45b0882bd2d425f65607d14ed3a463
firatyedibela <yedibelafirat0@gmail.com>
2024-10-16 01:33:30 +0300

    build to deploy

--- social-links-profile/vite.config.js
+++ social-links-profile/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/frontend-mentor-challenges/social-links-profile/',
+  base: '/social-links-profile/dist',
 });
